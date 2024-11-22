ee100ac83c253d725886afafbb21efe5e5c2cf3f
Haik <haik.asatryan.95@gmail.com>
2024-11-22 13:53:18 +0400

    added health check

---
+++ src/DistributedCache/Extensions/HealthCheckExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DistributedCache.Extensions;
+
+internal static class HealthCheckExtension
+{
+   internal static WebApplicationBuilder AddRedisHealthCheck(this WebApplicationBuilder builder, string connectionString)
+   {
+      builder.Services
+             .AddHealthChecks()
+             .AddRedis(connectionString, timeout: TimeSpan.FromSeconds(3));
+
+      return builder;
+   }
+}
--- src/DistributedCache/Options/CacheResetMode.cs
+++
@@ -1,7 +0,0 @@
-namespace DistributedCache.Options;
-
-internal enum CacheResetMode //Discontinued feature
-{
-   None = 1,
-   ResetFrequentTagsAfterHealthCheckFail = 2
-}
