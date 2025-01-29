936e5120c73b884bfae3cf6a5d088e083ca46b82
HaikAsatryan <haik.asatryan.95@gmail.com>
2025-01-29 13:34:26 +0400

    new extensions

--- src/DistributedCache/Extensions/StringExtensions.cs
+++ src/DistributedCache/Extensions/StringExtensions.cs
@@ -4,25 +4,28 @@ namespace DistributedCache.Extensions;
 
 public static class StringExtensions
 {
+
+   public static string PrefixWithAssemblyName(this string value)
+   {
+      var assembly = Assembly.GetCallingAssembly();
+      return value.PrefixWith(assembly.GetName()
+                                      .Name!);
+   }
+
+   public static List<string> PrefixWithAssemblyName(this IEnumerable<string> values)
+   {
+      var assembly = Assembly.GetCallingAssembly().GetName().Name;
+      return values.Select(x => x.PrefixWith(assembly!))
+                   .ToList();
+   }
+
    public static string PrefixWith(this string value, string prefix)
    {
       return $"{prefix}{value}";
    }
-
-   public static string PrefixWith(this string value, Assembly assembly)
-   {
-      return $"{assembly.GetName().Name}:{value}";
-   }
-
    public static List<string> PrefixWith(this IEnumerable<string> values, string prefix)
    {
       return values.Select(x => x.PrefixWith(prefix))
-                   .ToList();
-   }
-
-   public static List<string> PrefixWith(this IEnumerable<string> values, Assembly assembly)
-   {
-      return values.Select(x => x.PrefixWith(assembly))
                    .ToList();
    }
 }
